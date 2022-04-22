652fb2abcda69dc398a7cc3e2ab9289a4e5d0c47
Great Designer Software <45456833+JPscioli@users.noreply.github.com>
2022-04-22 19:50:27 -0300

    arquivos inicio

---
+++ restfull_ebook/src/main/java/com/example/restfull_ebook/RestfullEbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfull_ebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullEbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullEbookApplication.class, args);
+	}
+
+}
---
+++ restfull_ebook/src/test/java/com/example/restfull_ebook/RestfullEbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfull_ebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullEbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
